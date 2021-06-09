cba91ae44b4d9c4764020981baa36c1802403b5b
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-06-09 03:13:26 -0400

    streamlined functionality for changing location

--- body.py
+++ body.py
@@ -87,47 +87,42 @@ pidgeot = Pokemon("Pidgeot", 120, 120, pidgeot_attacks)
 pokemon_party = []
 options = [charizard, blastoise, venusaur, alakazam, snorlax, gyarados, machamp, electabuzz, pidgeot]
 
-trainer_name = input("Enter a name ")
-sean = Enemy("Team Rocket Leader Sean", "Sean: Spelling isn't your only nemesis today.", [persian, mewtwo])
-sampai = Enemy("Rocket Grunt Sam-pai", "Sam-pai: You're going nowhere! I've got you for 3 minutes.", [murkrow, arbok])
-zachary = Enemy("Rocket Grunt Zachary", "Zachary: Finally, something to do.", [koffing, houndoom])
-trainer = Trainer(trainer_name, pokemon_party, "Union Cave")
+
 
 # Location Options
-union_cave_options = ["Ecruteak City"]
-ecruteak_city_options = ["Pokemon Center", "Route 34", "Slowpoke Well", "Enter house with garden"]
-pokemon_center_options = ["Ecruteak City"]
-old_man_options = ["Ecruteak City"]
-route_34_options = ["Battle trainer on the left", "Battle trainer on the right", "Ecruteak City"]
-slowpoke_well_options = ["Walk deeper into well", "Ecruteak City"]
-main_room_options = ["Explore down the ladder", "Explore crack in the wall","Try the large metal door", "Slowpoke Well"]
-grunt_office_options = ["Battle Team Rocket Grunt", "Back to Slowpoke Well Entrance"]
-cracked_wall_options = ["Battle Team Rocket Grunt", "Back to Slowpoke Well Entrance"]
-sean_office_options = ["Ladder leading out to Ecruteak City", "Battle Team Rocket Leader"]
-
+union_cave_options = [1]
+ecruteak_city_options = [2, 4, 5, 3]
+pokemon_center_options = [1]
+old_man_options = [1]
+route_34_options = [1]
+slowpoke_well_options = [6, 1]
+main_room_options = [7, 8, 9, 5]
+grunt_office_options = [5]
+cracked_wall_options = [5]
+sean_office_options = [1]
 
 
 # Locations
 union_cave = Location("Union Cave", union_cave_options, "After several hours of wandering about, you finally navigate your way out of the dark, tricky cave. Ahead of you is a sign: Ecruteak City - 500 feet")
 ecruteak_city = Location("Ecruteak City", ecruteak_city_options, "You're in Ecruteak City. Birds are chirping and life is good.")
 pokemon_center = Location("Pokemon Center", pokemon_center_options, "You walk into the Pokemon Center. Nurse Joy greets you")
-old_man = Location("House with garden", old_man_options, "Old man: Hello youngster. Are you new to town? Keep a close eye on your pokemon, strange things have been happening around town lately.")
 route_34 = Location("Route 34", route_34_options, "You leave the city and are now on Route 34. There are trainers all around you battling")
+old_man = Location("House with garden", old_man_options, "Old man: Hello youngster. Are you new to town? Keep a close eye on your pokemon, strange things have been happening around town lately.")
 slowpoke_well = Location("Inside Slowpoke Well", slowpoke_well_options, "You are now inside the slowpoke well. The inside is a lot larger than you imagined. It has dim lighting and feels humid")
 main_room = Location("Deep Inside Slowpoke Well", main_room_options, "You are deep inside the well. You notice a ladder to your right leading down, and a fairly large crack in the wall to your left.")
 grunt_office = Location("Grunt Office", grunt_office_options, "You descend down the ladder and find yourself inside a room that looks like an office. There are slowpokes in the corner, each missing their tails. You spot a Team Rocket member at the end of the room, but he has yet to spot you.")
 cracked_wall = Location("Crack in the wall", cracked_wall_options, "You crawl through the crack in the wall and emerge into a large area. There are dozens of slowpoke hanging by a small body of water. You notice a Team Rocket member pushing a large cart.")
 sean_office = Location("Sean's office", sean_office_options, "You enter the room. Inside, you find the man in charge of the entire operation. He notices you, but does not react.")
+locations = [union_cave, ecruteak_city, pokemon_center, old_man, route_34, slowpoke_well, main_room, grunt_office, cracked_wall, sean_office]
 
 
-
-
-
-
-
-
-
-
+trainer_name = input("Enter a name ")
+sean = Enemy("Team Rocket Leader Sean", "Sean: Spelling isn't your only nemesis today.", [persian, mewtwo])
+sampai = Enemy("Rocket Grunt Sam-pai", "Sam-pai: You're going nowhere! I've got you for 3 minutes.", [murkrow, arbok])
+zachary = Enemy("Rocket Grunt Zachary", "Zachary: Finally, something to do.", [koffing, houndoom])
+# joey = Enemy("Younger Joey", "I just lost, so I'm trying to find more Pokemon. Wait! You look weak! Come on, lets battle!")
+# abe = Enemy("Birdkeeper Abe", "You're not as fly as me!")
+trainer = Trainer(trainer_name, pokemon_party, union_cave)
 
 
 
@@ -145,70 +140,86 @@ def choose_pokemon(trainer, choices):
   print("\033c")
 
 
+running = True
 
-def current_location(location):
-  print(f'Current Location: {location.name}\n')
-  print(f'{location.description}\n\n')
-  count = 0
-  while count < len(location.options):
-    print(f'{count + 1}. {location.options[count]}')
-    count += 1
-  location_choice = Validate().range(1, len(location.options), "Please choose a valid option", "Where would you like to go? ")
-  trainer.location = location.options[location_choice - 1]
-  print("\033c")
-
-
-def play_game():
+def play_game(trainer):
   logo_img()
-  input("Press enter to continue")
-  print("\033c")
-  choose_pokemon(trainer,options)
-  choose_pokemon(trainer,options)
-  choose_pokemon(trainer,options)
-  while True:
-    if trainer.location == 'Union Cave':
-      current_location(union_cave)
+  while running:
+    print(f'Current Location: {trainer.location.name}\n')
+    print(f'{trainer.location.description}\n\n')
+    count = 0
+    while count < len(trainer.location.options):
+      print(f'{count + 1}. {locations[trainer.location.options[count]].name}')
+      count += 1
+    location_choice = Validate().range(1, len(trainer.location.options), "Please choose a valid option", "Where would you like to go? ")
+    trainer.location = locations[trainer.location.options[location_choice - 1]]
+    print("\033c")
 
-    elif trainer.location == 'Ecruteak City':
-      current_location(ecruteak_city)
+play_game(trainer)
 
-    elif trainer.location == 'Pokemon Center':
-      current_location(pokemon_center)
+# def play_game():
+#   logo_img()
+#   input("Press enter to continue")
+#   print("\033c")
+#   choose_pokemon(trainer,options)
+#   choose_pokemon(trainer,options)
+#   choose_pokemon(trainer,options)
+#   while True:
+#     if trainer.location == 'Union Cave':
+#       current_location(union_cave)
 
-    elif trainer.location == "Enter house with garden":
-      current_location(old_man)
+#     elif trainer.location == 'Ecruteak City':
+#       current_location(ecruteak_city)
 
-    elif trainer.location == 'Route 34':
-      current_location(route_34)
+#     elif trainer.location == 'Pokemon Center':
+#       current_location(pokemon_center)
 
-    elif trainer.location == 'Slowpoke Well':
-      current_location(slowpoke_well)
+#     elif trainer.location == "Enter house with garden":
+#       current_location(old_man)
 
-    elif trainer.location == "Back to Slowpoke Well Entrance":
-      current_location(main_room)
+#     elif trainer.location == 'Route 34':
+#       current_location(route_34)
     
-    elif trainer.location == "Walk deeper into well":
-      current_location(main_room)
+#     elif trainer.location == 'Battle youngster trainer':
+#       joey_battle.battle()
+#       current_location(route_34)
+
+#     elif trainer.location == 'Battle birdkeeper trainer':
+#       abe_battle.battle()
+#       current_location(route_34)
+
+#     elif trainer.location == 'Slowpoke Well':
+#       current_location(slowpoke_well)
+
+#     elif trainer.location == "Back to Slowpoke Well Entrance":
+#       current_location(main_room)
     
-    elif trainer.location == "Explore down the ladder":
-      current_location(grunt_office)
+#     elif trainer.location == "Walk deeper into well":
+#       current_location(main_room)
     
-    elif trainer.location == "Explore crack in the wall":
-      current_location(cracked_wall)
-
-    elif trainer.location == "Try the large metal door":
-      current_location(sean_office)
-
-    elif trainer.location == "Battle Team Rocket Leader":
-      sean_battle.battle()
-      current_location(sean_office)
+#     elif trainer.location == "Explore down the ladder":
+#       current_location(grunt_office)
     
-    elif trainer.location == "Ladder leading out to Ecruteak City":
-      current_location(ecruteak_city)
+#     elif trainer.location == "Explore crack in the wall":
+#       current_location(cracked_wall)
+
+#     elif trainer.location == "Try the large metal door":
+#       current_location(sean_office)
+
+#     elif trainer.location == "Battle Team Rocket Leader":
+#       sean_battle.battle()
+#       current_location(sean_office)
+    
+#     elif trainer.location == "Ladder leading out to Ecruteak City":
+#       current_location(ecruteak_city)
 
 
-sean_battle = Battle(trainer, sean)
-play_game()
+# sean_battle = Battle(trainer, sean)
+# joey_battle = Battle(trainer, joey)
+# abe_battle = Battle(trainer, abe)
+# zachary_battle = Battle(trainer, zachary)
+# sam_battle = Battle(trainer, sampai)
+# play_game()
 
 
 # current_location(location)
